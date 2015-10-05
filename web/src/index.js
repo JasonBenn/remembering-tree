9b5e0c90eeb296957ab9d1ac42f5045e4d6bcca3
Jason Benn <jasoncbenn@gmail.com>
2015-10-04 17:18:14 -0700

    Got a simple Backbone setup on the page.

--- web/src/index.js
+++ web/src/index.js
@@ -1,2 +1,37 @@
+import $ from 'jquery'
+import _ from 'lodash'
+import { View, Model, Collection } from 'backbone'
+
 import 'normalize.css'
 import './styles/main.scss'
+
+const Node = Model
+const NodeView = View.extend({
+  template: _.template($('#node-template').html()),
+
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.render)
+  },
+
+  render: function() {
+    this.setElement(this.template(this.model.attributes))
+    return this
+  }
+})
+
+$(() => {
+  ['a', 'b', 'c', 'd'].forEach(word => {
+    const node = new Node({
+      title: word,
+      position: {
+        x: _.random(0, 150),
+        y: _.random(100, 200)
+      }
+    })
+    const view = new NodeView({
+      model: node
+    })
+
+    $('#main').append(view.render().el)
+  })
+})
